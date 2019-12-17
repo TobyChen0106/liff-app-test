ee26663580e5cc5bf69b12e925e42ee784a19423
TobyChen0106 <awesdjzo@gmail.com>
2019-12-17 14:57:11 +0800

    simple form test

--- liff-starter.js
+++ liff-starter.js
@@ -5,12 +5,12 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
+    // document.getElementById('languagefield').textContent = data.language;
+    // document.getElementById('viewtypefield').textContent = data.context.viewType;
+    // document.getElementById('useridfield').textContent = data.context.userId;
+    // document.getElementById('utouidfield').textContent = data.context.utouId;
+    // document.getElementById('roomidfield').textContent = data.context.roomId;
+    // document.getElementById('groupidfield').textContent = data.context.groupId;
 
     // openWindow call
     document.getElementById('openwindowbutton').addEventListener('click', function () {
@@ -85,4 +85,122 @@ function toggleElement(elementId) {
     } else {
         elem.style.display = "block";
     }
+}
+
+formOnSubmit = () => {
+    if (document.getElementById('agree').checked) { 
+        liff.closeWindow();
+        return true; 
+    } 
+    else { 
+        alert('請閱讀並同意使用紙服務條款!'); 
+        return false;
+    }
+}
+
+// slider
+var slider = new Slider(document.getElementById('slider'), 0, 100);
+slider.onChange = function(value) {
+    document.getElementById('value').textContent = Math.round(value);
+};
+slider.setValue(25);
+
+function Slider(container, minValue, maxValue) {
+    var slider = this;
+    
+    ///////////
+    //  DOM  //
+    ///////////
+    var slideGroup = document.createElement('div');
+    container.appendChild(slideGroup);
+    slideGroup.style.position = 'relative';
+    slideGroup.style.width =
+    slideGroup.style.height =
+        '100%';
+    
+    var slideBar = document.createElement('div');
+    slideGroup.appendChild(slideBar);
+    slideBar.style.position = 'absolute';
+    slideBar.style.left =
+    slideBar.style.right =
+    slideBar.style.top =
+    slideBar.style.bottom =
+        Math.round(container.offsetHeight / 2 - 1) + 'px';
+    slideBar.style.backgroundColor = 'black';
+    
+    var slideButton = document.createElement('div');
+    slideGroup.appendChild(slideButton);
+    slideButton.style.position = 'absolute';
+    slideButton.style.width =
+    slideButton.style.height =
+    slideButton.style.borderRadius =
+        container.offsetHeight + 'px';
+    
+    /////////////
+    //  COLOR  //
+    /////////////
+    var startColor = {r: 255, g: 0, b: 0};
+    var midColor = {r: 255, g: 255, b: 0};
+    var endColor = {r: 0, g: 255, b: 0};
+    
+    var colorAt = function(position) {
+        if (position <= .5) {
+            var r = startColor.r * (.5 - position) * 2 + midColor.r * (position - 0) * 2;
+            var g = startColor.g * (.5 - position) * 2 + midColor.g * (position - 0) * 2;
+            var b = startColor.b * (.5 - position) * 2 + midColor.b * (position - 0) * 2;
+        } else {
+            var r = midColor.r * (1 - position) * 2 + endColor.r * (position - .5) * 2;
+            var g = midColor.g * (1 - position) * 2 + endColor.g * (position - .5) * 2;
+            var b = midColor.b * (1 - position) * 2 + endColor.b * (position - .5) * 2;
+        }
+        return 'rgb('+Math.ceil(r)+', '+Math.ceil(g)+', '+Math.ceil(b)+')';
+    };
+    
+    /////////////
+    //  VALUE  //
+    /////////////
+    var value = null;
+    
+    slider.getValue = function() {
+        return value;
+    };
+    
+    slider.setValue = function(newValue) {
+        value = Math.max(minValue, Math.min(maxValue, newValue));
+        var position = (value - minValue) / (maxValue - minValue);
+        slideButton.style.left = Math.round(position * slideBar.offsetWidth) + 'px';
+        slideButton.style.backgroundColor = colorAt(position);
+        if (slider.onChange) slider.onChange(value);
+    };
+    
+    slider.setValue(minValue);
+    
+    /////////////
+    //  MOUSE  //
+    /////////////
+    var sliding = false;
+    var startX = 0;
+    
+    document.addEventListener('mousedown', function(event) {
+        if (event.target === slideButton) {
+            event.preventDefault();
+            sliding = true;
+            startX = event.pageX;
+        }
+    });
+    
+    document.addEventListener('mouseup', function(event) {
+        if (sliding) {
+            sliding = false;
+            startX = null;
+        }
+    });
+    
+    document.addEventListener('mousemove', function(event) {
+        if (sliding) {
+            var newValue = value + ((event.pageX - startX) / slideBar.offsetWidth) * (maxValue - minValue);
+            startX = event.pageX;
+            slider.setValue(newValue);
+        }
+    });
 }
